ca7652ede5018f859a827400c5c8e54df4df264d
kiruthigaranik <kiruthigaranik@gmail.com>
2025-12-31 14:21:17 +0530

    add link to menus using Link component

--- src/component/HeaderComponent.js
+++ src/component/HeaderComponent.js
@@ -2,6 +2,8 @@ import  React , {useState} from  "react";
 import  "../css/index.css";
 import {logo} from '../images/logo.png';
 
+import { Link } from "react-router";
+
 
 const Header = () => {
     const [btnName, setBtnName] = useState("LOGIN");
@@ -12,14 +14,15 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li> <Link to="/">Home</Link></li>
+                    <li> <Link to="/about">About</Link></li>
+                    <li> <Link to="/contact">Contact</Link></li>
                     <li>Blog</li>
                     <li>Careers</li>
                     <button className="login-btn" onClick={(()=>{
                         ( btnName === "LOGIN" ? setBtnName("LOGOUT") : setBtnName("LOGIN"));
                     })}>{btnName}</button>
+                   
                 </ul>
                
             </div>
