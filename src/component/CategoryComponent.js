cca627d2d0c8ec24a613e8ee0f26ca9a167cb319
kiruthigaranik <kiruthigaranik@gmail.com>
2026-01-07 14:43:37 +0530

    lifting sttate up: fixed the toggle issue

--- src/component/CategoryComponent.js
+++ src/component/CategoryComponent.js
@@ -1,8 +1,10 @@
 
 import CategoryDetail from "./CategoryDetail";
 
-const CategoryComponent = ({category, showIndex, showItem}) => {
 
+const CategoryComponent = ({category, index, isOpen, toggle }) => {
+
+  
   // const [isOpen, setIsOpen] = useState(false); //this will help to toggle the category items, but it wont collapse other opened categories
   /**
    * Instead of above state in CategoryComponent, we can lift the state up to MenuComponent and pass the state and setState function as props to CategoryComponent
@@ -12,23 +14,31 @@ const CategoryComponent = ({category, showIndex, showItem}) => {
 
   const clickHandler = () => {
    showItem(showIndex);
+ 
   }
 
   return (
     <>
-    <div onClick={clickHandler} className="category-card border-0 border-gray-300 rounded-lg  w-6/12 mx-auto my-2 p-2 flex justify-between items-center bg-gray-100 hover:bg-gray-300 cursor-pointer">
+    <div onClick={toggle}
+        role="button"
+        tabIndex={index}
+        aria-expanded={isOpen}
+    
+    className="category-card border-0 border-gray-300 rounded-lg  w-6/12 mx-auto my-2 p-2 flex justify-between items-center bg-gray-100 hover:bg-gray-300 cursor-pointer">
         <span className="font-bold font-medium">{category.title} ({category["itemCards"].length})</span>
         <span >⬇️</span>
       </div>
+       {isOpen && (
       <div className="category-items-container ">
        
         {
           category["itemCards"].map((item) => {
              const processsedData = item?.card?.info;
-             return showIndex && <CategoryDetail key={processsedData?.id} CategoryDetail={processsedData} />
+             return <CategoryDetail key={processsedData?.id} CategoryDetail={processsedData} />
           })
         }
         </div>
+         )}
     </>
   )
 }
--- src/component/MenuComponent.js
+++ src/component/MenuComponent.js
@@ -5,12 +5,15 @@ import React, { useState } from "react";
 const MenuComponent = () => {
   const { info, listOfMenuData } = useFetchMenu();
   const [showIndex, setShowIndex] = useState(null);
+  const [openIndex, setOpenIndex] = useState(null);
   const isOnline = useOnlineStatus();
   
    const {name,avgRating, costForTwoMessage, cuisines} = info;
    
    //filter ItemCategory only from listOfMenuData
-
+const handleToggle = (index) => {
+  setOpenIndex(prev => (prev === index ? null : index));
+};
    const filterItemCategory = listOfMenuData.filter ((category) =>{
     return category?.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     
@@ -24,8 +27,12 @@ const MenuComponent = () => {
       {
         filterItemCategory.map((category,index) => (
           <CategoryComponent key={category?.card?.["card"]?.categoryId} category={category?.card?.card} 
-            showIndex={index === showIndex ? true : false}
-            showItem={() => setShowIndex(index)}/>
+         
+            index={index}
+            isOpen={openIndex === index}
+            toggle={() => handleToggle(index)}
+            
+            />
         ))
       }
      
