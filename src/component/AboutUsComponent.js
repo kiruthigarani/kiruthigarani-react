bfeb2c24a022b0c854a6d5eda4f533642e8a08a0
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-01-08 23:32:26 +0530

    useMemo example

--- src/component/AboutUsComponent.js
+++ src/component/AboutUsComponent.js
@@ -1,6 +1,15 @@
-import React from 'react';
-
+import React, {useState,useMemo} from 'react';
+import findNthPrime from '../utils/calcPrime';
 export default function AboutUsComponent() {
+    const [heavyCalculation, setHeavyCalculation] = useState("");
+    const [showTheme, setShowThem] = useState(true);
+
+    console.log("AboutUsComponent Rendered");
+
+    const prime = useMemo(()=>{
+        console.log("Calculating prime number...",heavyCalculation);
+        return findNthPrime(Number(heavyCalculation));
+    },[heavyCalculation]);
     return (
         <div className="about-us">
             <h1>About Us</h1>
@@ -20,6 +29,18 @@ export default function AboutUsComponent() {
                 software development, design, and digital strategy. We work collaboratively
                 to bring your vision to life and drive your business forward.
             </p>
+
+        <div>
+                <h2>UeMemo</h2>
+                <div className={`border-2 border-black m-2 p-2 h-64 ${showTheme ? 'bg-gray-800' : ''}`}>
+                    <button className='m-2 p-2 border-2 bg-green-200' onClick={() => setShowThem(!showTheme)}>Toggle</button>
+                    <input className='border-2' name="heavy_calculattion" type="number" onChange={(e) =>{setHeavyCalculation(e.target.value)}} value={heavyCalculation} />
+
+                    <p>{heavyCalculation && `The ${heavyCalculation}th prime number is ${prime}`}</p>
+                </div>
         </div>
+        </div>
+
+        
     );
 }
