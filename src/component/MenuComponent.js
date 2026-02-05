aed0455df931610f3abc6d9520b0b20c7da7243f
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-02-05 11:03:15 +0530

    traffic light

--- src/component/MenuComponent.js
+++ src/component/MenuComponent.js
@@ -18,6 +18,7 @@ const MenuComponent = () => {
     
     });
     };
+   
    const filterItemCategory = listOfMenuData.filter ((category) =>{
     return category?.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     
