f95b93587360841f0b991ac41afb7d525c5c68f9
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-03-13 15:24:48 +0530

    added utils folder

---
+++ src/utils/useFetchMenu.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import menuresponse from "../menuResponse.json";
+const useFetchMenu = () => {
+    const restaurantId = useParams().resId;
+
+  //   useEffect(() => {
+  //   fetchMenuData();
+  // }, []);
+
+  //  const fetchMenuData =  async() => {
+  //   const data =  await fetch(
+  //     `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.9435367&lng=78.0537813&restaurantId=${restaurantId}`,
+  //   );
+  //   console.log("Menu data fetched",data);
+  //   //const json =  data.json();
+    
+  // };
+
+  const menuData = menuresponse[0].data.cards;
+    const listOfMenuData =menuData[4].groupedCard.cardGroupMap.REGULAR.cards;
+  console.log("Menu data-listOfMenuData:", listOfMenuData);
+  const listOfRecommItems = listOfMenuData[1].card.card.itemCards;
+ console.log("Menu data:", listOfRecommItems);
+ const info = menuData[2].card.card.info;
+
+  return { info, listOfMenuData };
+}
+export default useFetchMenu;
---
+++ src/utils/useOnlineStatus.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useOnlineStatus = () => {
+  const [isOnline, setIsOnline] = useState(() => {
+    return typeof navigator !== "undefined" ? navigator.onLine : true;
+  });
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    // cleanup
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+};
+
+export default useOnlineStatus;
